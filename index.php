2f294d28cb824452005e704595b8f4dc37ebbddb
salazarcode <salazarcode@gmail.com>
2019-12-18 17:39:09 -0500

    Última actualización en el servidor

--- index.php
+++ index.php
@@ -7,12 +7,24 @@ class Coordenada
     public $Categoria3 = "";
     public $Latitude = "";
     public $Longitude = "";
+    public $City = "";
+    public $Country = "";
 
-    function __construct($_autor, $_extra) {
+    function __construct($_autor, $_extra, $_lugar) {
         $elements = explode(":", $_autor);
         $this->Categoria1  = trim($elements[0]);
         $this->Categoria2  = trim($elements[1]);
         $this->Categoria3  = trim($elements[2]);
+        
+        if($_lugar!="")
+        {
+            $place = explode(",", $_lugar);
+            $this->City  = trim($place[0]);
+            if(trim($place[1]) != "" )
+                $this->Country  = trim($place[1]); 
+            else
+                $this->Country  = trim($place[2]);
+        }   
 
         if($_extra!="")
         {
@@ -24,10 +36,10 @@ class Coordenada
     }
 }
 
-$servername = "192.185.82.200";
-$username = "bands7ft_maps";
+$servername = "192.185.166.159";
+$username = "sur74vis_map";
 $password = "Java***174";
-$db = "bands7ft_db";
+$db = "sur74vis_db";
 
 
 $conn = new mysqli($servername, $username, $password, $db);
@@ -36,7 +48,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select autor, extra from articulo where cat_id = 22 and cat_nivel = 1";
+$sql = "select autor, extra, lugar from articulo where cat_id = 22 and cat_nivel = 1";
 $result = $conn->query($sql);
 
 $resArray = [];
@@ -45,7 +57,7 @@ if (mysqli_num_rows($result) > 0)
 {
     while($row = mysqli_fetch_assoc($result)) 
     {
-        $obj = new Coordenada($row["autor"], $row["extra"]);
+        $obj = new Coordenada($row["autor"], $row["extra"], $row["lugar"]);
         $resArray[] = $obj;
     }
 } 
@@ -54,6 +66,8 @@ else
     echo "0 results";
 }
 
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 echo json_encode($resArray);
 
 $conn->close();
